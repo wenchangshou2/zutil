d29ecb3b7af893181be9f2ce20d9740ff74fbbdc
wcs <wenchangshou@live.cn>
2021-02-24 13:50:10 +0800

    update

--- file.go
+++ file.go
@@ -13,27 +13,27 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
-
 )
 
-func IsExist(path string)bool{
-	_,err:=os.Stat(path)
-	return err==nil||os.IsExist(err)
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
 }
 
-func GetResourceAbsolutePath(rPath string,id string)string{
-	return path.Join(rPath,id)
+func GetResourceAbsolutePath(rPath string, id string) string {
+	return path.Join(rPath, id)
 }
-func IsAbsolutePath(p string)bool{
+func IsAbsolutePath(p string) bool {
 	return path.IsAbs(p)
 }
-func GetSize(f multipart.File)(int ,error){
-	content,err:=ioutil.ReadAll(f)
-	return len(content),err
+func GetSize(f multipart.File) (int, error) {
+	content, err := ioutil.ReadAll(f)
+	return len(content), err
 }
-func GetExt(fileName string)string{
+func GetExt(fileName string) string {
 	return path.Ext(fileName)
 }
 func CheckExist(src string) bool {
@@ -50,29 +50,30 @@ func CheckPermission(src string) bool {
 
 func IsNotExistMkDir(src string) error {
 	if exist := CheckExist(src); exist == true {
-		if err := os.MkdirAll(src,0755); err != nil {
+		if err := os.MkdirAll(src, 0755); err != nil {
 			return err
 		}
 	}
 
 	return nil
 }
-func ReMkdir(src string)error{
-	if err:=IsExistDelete(src);err!=nil{
+func ReMkdir(src string) error {
+	if err := IsExistDelete(src); err != nil {
 		return err
 	}
 
-	if err:=IsNotExistMkDir(src);err!=nil{
-		return  err
+	if err := IsNotExistMkDir(src); err != nil {
+		return err
 	}
 	return nil
 }
+
 //如果目录存在就直接删除
-func IsExistDelete(src string) error{
-	if exist:=CheckExist(src);!exist{
+func IsExistDelete(src string) error {
+	if exist := CheckExist(src); !exist {
 		os.RemoveAll(src)
 	}
-	return  nil
+	return nil
 }
 
 func MkDir(src string) error {
@@ -117,32 +118,33 @@ func MustOpen(fileName, filePath string) (*os.File, error) {
 
 	return f, nil
 }
-func substr(s string,pos,length int)string{
-	runes:=[]rune(s)
-	l:=pos+length
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
 
-	if l>len(runes){
-		l=len(runes)
+	if l > len(runes) {
+		l = len(runes)
 	}
 	return string(runes[pos:l])
 }
-func getParentDirector(director string) string{
-	director=strings.ReplaceAll(director,"\\","/")
-	return  substr(director,0,strings.LastIndex(director,"/"))
+func getParentDirector(director string) string {
+	director = strings.ReplaceAll(director, "\\", "/")
+	return substr(director, 0, strings.LastIndex(director, "/"))
 }
+
 //解析zip文件
 //sourcePath:源文件
 //targetPath:目标路径
-func Unzip(sourcePath,targetPath string)(err error){
-	zipReader,err:=zip.OpenReader(sourcePath)
+func Unzip(sourcePath, targetPath string) (err error) {
+	zipReader, err := zip.OpenReader(sourcePath)
 	defer zipReader.Close()
-	if err!=nil{
+	if err != nil {
 		return
 	}
-	for _,file:=range zipReader.Reader.File{
-		zippedFile,err:=file.Open()
+	for _, file := range zipReader.Reader.File {
+		zippedFile, err := file.Open()
 		defer zippedFile.Close()
-		if err!=nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 
@@ -150,66 +152,67 @@ func Unzip(sourcePath,targetPath string)(err error){
 			simplifiedchinese.GBK.NewDecoder())
 		fileName, err := ioutil.ReadAll(utf8Reader)
 
-		extractedFilePath:=filepath.Join(
-			targetPath,string(fileName),
+		extractedFilePath := filepath.Join(
+			targetPath, string(fileName),
 		)
-		if file.FileInfo().IsDir(){ //目录处理
-			os.MkdirAll(extractedFilePath,file.Mode())
-		}else{ //文件处理
-			parentPath:=getParentDirector(extractedFilePath) //获取上一级目录
-			IsNotExistMkDir(parentPath)//如果上一级目录不存在就自动创建
-			outputFile,err:=os.OpenFile(
+		if file.FileInfo().IsDir() { //目录处理
+			os.MkdirAll(extractedFilePath, file.Mode())
+		} else { //文件处理
+			parentPath := getParentDirector(extractedFilePath) //获取上一级目录
+			IsNotExistMkDir(parentPath)                        //如果上一级目录不存在就自动创建
+			outputFile, err := os.OpenFile(
 				extractedFilePath,
 				os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
-				file.Mode())//创建输出文件
-			if err!=nil{
+				file.Mode()) //创建输出文件
+			if err != nil {
 				log.Fatal(err)
 			}
 			defer outputFile.Close()
-			_,err=io.Copy(outputFile,zippedFile)//将压缩包内的数据写入到目标文件
-			if err!=nil{
+			_, err = io.Copy(outputFile, zippedFile) //将压缩包内的数据写入到目标文件
+			if err != nil {
 				log.Fatal(err)
 			}
 		}
 	}
 	return
 }
+
 //获取文件名
-func GetFileName(path string)string{
-	idx:=strings.LastIndex(path,"/")
-	if idx==-1{
+func GetFileName(path string) string {
+	idx := strings.LastIndex(path, "/")
+	if idx == -1 {
 		return ""
 	}
 	return path[idx+1:]
 }
-func GetFileExt(p string)string{
+func GetFileExt(p string) string {
 	return filepath.Ext(p)
 }
-func CopyFile(source,target string)(error){
-	sfi,err:=os.Stat(source)
-	if err!=nil{
+func CopyFile(source, target string) error {
+	sfi, err := os.Stat(source)
+	if err != nil {
 		return err
 	}
-	if !sfi.Mode().IsRegular(){
+	if !sfi.Mode().IsRegular() {
 		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
 	}
-	dfi,err:=os.Stat(target)
-	if err!=nil{
-		if !os.IsNotExist(err){
-			return  err
+	dfi, err := os.Stat(target)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
 		}
-	}else{
-		if !(dfi.Mode().IsRegular()){
-			return fmt.Errorf("CopyFile:no-regular destination file%s (%q)",dfi.Name(),dfi.Mode().String())
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile:no-regular destination file%s (%q)", dfi.Name(), dfi.Mode().String())
 		}
-		if os.SameFile(sfi,dfi){
+		if os.SameFile(sfi, dfi) {
 			return nil
 		}
 	}
-	if err=os.Link(source,target);err==nil{
+	if err = os.Link(source, target); err == nil {
 		return nil
 	}
-	err=copyFileContents(source,target)
+	err = copyFileContents(source, target)
 
 	return err
 }
@@ -235,40 +238,53 @@ func copyFileContents(src, dst string) (err error) {
 	err = out.Sync()
 	return
 }
+
 //获取文件的md5
-func GetFileMd5(filePath string)(string,error){
+func GetFileMd5(filePath string) (string, error) {
 	var returnMD5String string
-	file,err:=os.Open(filePath)
-	if err!=nil{
-		return returnMD5String,err
+	file, err := os.Open(filePath)
+	if err != nil {
+		return returnMD5String, err
 	}
 	defer file.Close()
-	hash:=md5.New()
-	if _,err:=io.Copy(hash,file);err!=nil{
-		return returnMD5String,nil
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMD5String, nil
 	}
-	hashInBytes:=hash.Sum(nil)[:16]
-	returnMD5String=hex.EncodeToString(hashInBytes)
-	return returnMD5String,nil
+	hashInBytes := hash.Sum(nil)[:16]
+	returnMD5String = hex.EncodeToString(hashInBytes)
+	return returnMD5String, nil
 }
 
-
-func GetWorkDirFileListRecursive(workDir string) ([]string,error){
-	var files[]string
-	err:=filepath.Walk(workDir, func(path string, info os.FileInfo, err error) error {
-		files=append(files,path)
+func GetWorkDirFileListRecursive(workDir string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(workDir, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
 		return nil
 	})
-	return files,err
+	return files, err
 }
-func GetWorkDirFileList(workDir string)(fileList []string,err error){
-	files,err:=ioutil.ReadDir(workDir)
-	if err!=nil{
+func GetWorkDirFileList(workDir string) (fileList []string, err error) {
+	files, err := ioutil.ReadDir(workDir)
+	if err != nil {
 		return
 	}
-	for _,f:=range files{
-		fileName:=path.Join(workDir,f.Name())
-		fileList=append(fileList,fileName)
+	for _, f := range files {
+		fileName := path.Join(workDir, f.Name())
+		fileList = append(fileList, fileName)
 	}
 	return
+}
+
+// CreatNestedFile 给定path创建文件，如果目录不存在就递归创建
+func CreatNestedFile(path string) (*os.File, error) {
+	basePath := filepath.Dir(path)
+	if !IsExist(basePath) {
+		err := os.MkdirAll(basePath, 0700)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return os.Create(path)
 }
