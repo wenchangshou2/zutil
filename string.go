65bcaf141271b90757455c0e398995bd4a0fd248
wcs <wenchangshou@live.cn>
2021-02-24 13:44:48 +0800

    添加RandStringRunes

--- string.go
+++ string.go
@@ -1,30 +1,43 @@
 package zutil
 
 import (
+	"math/rand"
 	"reflect"
 	"strconv"
 )
 
-func MapToString(arguments map[string]interface{})string{
-	str:=""
-	for k,v:=range arguments{
-		s:=""
-		v:=reflect.ValueOf(v)
-		switch v.Kind(){
-		case reflect.Int,reflect.Int8,reflect.Int16,reflect.Int32,reflect.Int64:
-			s+=strconv.FormatInt(v.Int(),10)
-		case reflect.Uint,reflect.Uint8,reflect.Uint16,reflect.Uint32,reflect.Uint64,reflect.Uintptr:
-			s+=strconv.FormatUint(v.Uint(),10)
+// MapToString 参数识别
+func MapToString(arguments map[string]interface{}) string {
+	str := ""
+	for k, v := range arguments {
+		s := ""
+		v := reflect.ValueOf(v)
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			s += strconv.FormatInt(v.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			s += strconv.FormatUint(v.Uint(), 10)
 		case reflect.Bool:
-			s+=strconv.FormatBool(v.Bool())
+			s += strconv.FormatBool(v.Bool())
 		case reflect.String:
-			s+=strconv.Quote(v.String())
-		case reflect.Float32,reflect.Float64:
-			s+=strconv.Itoa(int(v.Float()))
+			s += strconv.Quote(v.String())
+		case reflect.Float32, reflect.Float64:
+			s += strconv.Itoa(int(v.Float()))
 		}
-		if len(s)>0{
-			str+=" -"+k+" "+s
+		if len(s) > 0 {
+			str += " -" + k + " " + s
 		}
 	}
 	return str
+}
+
+// RandStringRunes 返回随机字符串
+func RandStringRunes(n int) string {
+	var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
