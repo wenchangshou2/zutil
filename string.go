3a826d387efbe13f5c24a6dff5e30b36943f019c
changshou wen <wenchangshou@gmail.com>
2023-01-05 18:30:56 +0800

    添加--参数

--- string.go
+++ string.go
@@ -5,25 +5,52 @@ import (
 	"strconv"
 )
 
-func MapToString(arguments map[string]interface{})string{
-	str:=""
-	for k,v:=range arguments{
-		s:=""
-		v:=reflect.ValueOf(v)
-		switch v.Kind(){
-		case reflect.Int,reflect.Int8,reflect.Int16,reflect.Int32,reflect.Int64:
-			s+=strconv.FormatInt(v.Int(),10)
-		case reflect.Uint,reflect.Uint8,reflect.Uint16,reflect.Uint32,reflect.Uint64,reflect.Uintptr:
-			s+=strconv.FormatUint(v.Uint(),10)
+func MapToString(arguments map[string]interface{}) string {
+	str := ""
+	for k, v := range arguments {
+		s := ""
+		v := reflect.ValueOf(v)
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			s += strconv.FormatInt(v.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			s += strconv.FormatUint(v.Uint(), 10)
 		case reflect.Bool:
-			s+=strconv.FormatBool(v.Bool())
+			s += strconv.FormatBool(v.Bool())
 		case reflect.String:
-			s+=strconv.Quote(v.String())
-		case reflect.Float32,reflect.Float64:
-			s+=strconv.Itoa(int(v.Float()))
+			s += strconv.Quote(v.String())
+		case reflect.Float32, reflect.Float64:
+			s += strconv.Itoa(int(v.Float()))
 		}
-		if len(s)>0{
-			str+=" -"+k+" "+s
+		if len(s) > 0 {
+			str += " -" + k + " " + s
+		}
+	}
+	return str
+}
+func MapToStringV2(arguments map[string]interface{}) string {
+	str := ""
+	for k, v := range arguments {
+		s := ""
+		v := reflect.ValueOf(v)
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			s += strconv.FormatInt(v.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			s += strconv.FormatUint(v.Uint(), 10)
+		case reflect.Bool:
+			s += strconv.FormatBool(v.Bool())
+		case reflect.String:
+			s += strconv.Quote(v.String())
+		case reflect.Float32, reflect.Float64:
+			s += strconv.Itoa(int(v.Float()))
+		}
+		if len(s) > 0 {
+			if len(k) > 1 {
+				str += " --" + k + " " + s
+			} else {
+				str += " -" + k + " " + s
+			}
 		}
 	}
 	return str
